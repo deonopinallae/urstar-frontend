e05ec7b60957b28b09343dcc8bcb31b9f68e02a0
deonopinallae <deonopinallae@yandex.ru>
2025-12-03 16:16:41 +0300

    fix edit product

--- src/actions/save-product-async.ts
+++ src/actions/save-product-async.ts
@@ -1,19 +1,19 @@
+import { request } from '../utils'
 import { setProductData } from './set-product-data'
 import { setProducts } from './set-products'
 
 export const saveProductAsync = (id, newProductData, products) => async (dispatch) => {
-	try {
-		fetch(`https://urstar-backend.onrender.com/api/products/${id}/edit`, {
-			method: 'PATCH',
-			body: newProductData,
-			credentials: 'include',
-		}).then(res => res.json()).then(({updatedProduct}) => {
+	const isFormData = true
+	request(`/api/products/${id}/edit`, 'PATCH', { newProductData }, isFormData)
+		.then((res) => res.json())
+		.then(({ updatedProduct }) => {
 			dispatch(setProductData(updatedProduct))
-			dispatch(setProducts(products.map(p => p.id === updatedProduct.id ? updatedProduct : p)))
-
+			dispatch(
+				setProducts(
+					products.map((p) =>
+						p.id === updatedProduct.id ? updatedProduct : p,
+					),
+				),
+			)
 		})
-	} catch (err) {
-		console.error('product edit error:', err)
-		throw err
-	}
 }
