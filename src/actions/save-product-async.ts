8ab3e4e51af5bbd0d96d246f741b319471802a19
deonopinallae <deonopinallae@yandex.ru>
2025-12-03 16:04:50 +0300

    fix

--- src/actions/save-product-async.ts
+++ src/actions/save-product-async.ts
@@ -3,10 +3,10 @@ import { setProducts } from './set-products'
 
 export const saveProductAsync = (id, newProductData, products) => async (dispatch) => {
 	try {
-		fetch(`/api/products/${id}/edit`, {
+		fetch(`https://urstar-backend.onrender.com/api/products/${id}/edit`, {
 			method: 'PATCH',
 			body: newProductData,
-			// credentials: 'include',
+			credentials: 'include',
 		}).then(res => res.json()).then(({updatedProduct}) => {
 			dispatch(setProductData(updatedProduct))
 			dispatch(setProducts(products.map(p => p.id === updatedProduct.id ? updatedProduct : p)))
