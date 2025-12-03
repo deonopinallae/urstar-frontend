d6436c0bfd168ab1dc6db5a976c6b3d1c91b04a2
deonopinallae <deonopinallae@yandex.ru>
2025-12-03 16:29:33 +0300

    fix

--- src/actions/save-product-async.ts
+++ src/actions/save-product-async.ts
@@ -1,18 +1,33 @@
-import { request } from '../utils'
 import { setProductData } from './set-product-data'
 import { setProducts } from './set-products'
 
 export const saveProductAsync = (id, newProductData, products) => async (dispatch) => {
-	const isFormData = true
-	request(`/api/products/${id}/edit`, 'PATCH', { newProductData }, isFormData)
-		.then(({ updatedProduct }) => {
+	try {
+		fetch(`https://urstar-backend.onrender.com/api/products/${id}/edit`, {
+			method: 'PATCH',
+			body: newProductData,
+			credentials: 'include',
+		}).then(res => res.json()).then(({updatedProduct}) => {
 			dispatch(setProductData(updatedProduct))
-			dispatch(
-				setProducts(
-					products.map((p) =>
-						p.id === updatedProduct.id ? updatedProduct : p,
-					),
-				),
-			)
+			dispatch(setProducts(products.map(p => p.id === updatedProduct.id ? updatedProduct : p)))
+
 		})
+	} catch (err) {
+		console.error('product edit error:', err)
+		throw err
+	}
 }
+// export const saveProductAsync = (id, newProductData, products) => async (dispatch) => {
+// 	const isFormData = true
+// 	request(`/api/products/${id}/edit`, 'PATCH', { newProductData }, isFormData)
+// 		.then(({ updatedProduct }) => {
+// 			dispatch(setProductData(updatedProduct))
+// 			dispatch(
+// 				setProducts(
+// 					products.map((p) =>
+// 						p.id === updatedProduct.id ? updatedProduct : p,
+// 					),
+// 				),
+// 			)
+// 		})
+// }
